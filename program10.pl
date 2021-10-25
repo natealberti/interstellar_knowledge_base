dfd15fcb149a35f641ed799b1f9bf4d2f6f208cb
Nate <65456795+natealberti@users.noreply.github.com>
2021-10-24 17:54:40 -0700

    Add files via upload

---
+++ program10.pl
@@ -0,0 +1,24 @@
+interstellar( interstellar, 2014, christopher_nolan ).
+
+matthew_mcconaughe( matthew_mcconaughe, uvalde, 1969, alive ).
+cooper( matthew_mcconaughe, cooper, interstellar ).
+uvalde( uvalde, texas, united_states, 15751 ).
+
+anne_hathaway( anne_hathaway, brooklyn, 1982, alive ).
+dr_brand( anne_hathaway, dr_brand, interstellar ).
+brooklyn( brooklyn, new_york, united_states, 2590000 ).
+
+bill_irwin( bill_irwin, santa_monica, 1950, alive ).
+tars( bill_irwin, tars, interstellar ).
+santa_monica( santa_monica, california, united_states, 91577 ).
+
+jessica_chastain( jessica_chastain, sacramento, 1977, alive ).
+murph( jessica_chastain, murph, interstellar ).
+sacramento( sacramento, california, united_states, 500930 ).
+
+matt_damon( matt_damon, cambridge, 1970, alive ).
+dr_mann( matt_damon, dr_mann, interstellar ).
+cambridge( cambridge, texas, united_states, 116132 ).
+
+parentOf( cooper, murph ).
+
